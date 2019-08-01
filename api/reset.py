85c0c8ce9541e94d1ab3f521181a6104b5b56439
willcl-ark <will8clark@gmail.com>
2019-08-01 23:13:24 +0100

    add @check_connection
    Signed-off-by: willcl-ark <will8clark@gmail.com>

--- api/reset.py
+++ api/reset.py
@@ -4,16 +4,16 @@ from flask_restful import Api, Resource, reqparse
 
 import config
 import goTenna
+from utilities import check_connection
 
 
 class Reset(Resource):
     def __init__(self):
         super(Reset, self).__init__()
 
-    def post(self):
-        if config.connection is None:
-            status = "Connection not initialised, nothing to reset"
-        elif hasattr(config.connection, "api_thread") and isinstance(
+    @check_connection
+    def get(self):
+        if hasattr(config.connection, "api_thread") and isinstance(
             config.connection.api_thread, goTenna.driver.Driver
         ):
             config.connection.reset()
