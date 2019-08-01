c413f653f6bd007b88de6b843ef4cb199f88a203
willcl-ark <will8clark@gmail.com>
2019-08-01 23:15:58 +0100

    add send_broadcast and optimise imports
    Signed-off-by: willcl-ark <will8clark@gmail.com>

--- api/server.py
+++ api/server.py
@@ -1,24 +1,31 @@
 #!flask/bin/python
 
-from flask import Flask, jsonify, abort, make_response
-from flask_restful import Api, Resource, reqparse, fields, marshal
+import logging
+
+from flask import Flask
 from flask_httpauth import HTTPBasicAuth
+from flask_restful import Api
 
 from reset import Reset
+from sdk_token import SdkToken
+from send_broadcast import SendBroadcast
 from set_geo_region import SetGeoRegion
 from set_gid import SetGid
-from sdk_token import SdkToken
+
+logger = logging.getLogger(__name__)
+FORMAT = "[%(asctime)s - %(levelname)s] - %(message)s"
+logging.basicConfig(level=logging.DEBUG, format=FORMAT)
 
 app = Flask(__name__)
 api = Api(app)
 auth = HTTPBasicAuth()
 connection = None
 
-
 api.add_resource(Reset, "/gotenna/api/v1.0/reset")
+api.add_resource(SdkToken, "/gotenna/api/v1.0/sdk_token")
+api.add_resource(SendBroadcast, "/gotenna/api/v1.0/send_broadcast")
 api.add_resource(SetGeoRegion, "/gotenna/api/v1.0/set_geo_region")
 api.add_resource(SetGid, "/gotenna/api/v1.0/set_gid")
-api.add_resource(SdkToken, "/gotenna/api/v1.0/sdk_token")
 
 if __name__ == "__main__":
     app.run(debug=True)
