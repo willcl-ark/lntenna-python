479e25120f7eed2cfd737d37a725e2d12f444b55
willcl-ark <will8clark@gmail.com>
2019-08-08 20:39:08 +0100

    send_api_request will now parse all API requests and return .text val
    Signed-off-by: willcl-ark <will8clark@gmail.com>

--- lntenna/api/send_api_req.py
+++ lntenna/api/send_api_req.py
@@ -1,11 +1,6 @@
 #!flask/bin/python
 
 import ast
-import logging
-
-logger = logging.getLogger(__name__)
-
-import simplejson as json
 import requests
 from flask_restful import Resource, reqparse
 
@@ -13,72 +8,34 @@ from lntenna.gotenna_core.utilities import prepare_api_request
 
 
 class ApiRequest(Resource):
+    """All api requests are called via 'POST'
+
+    Takes a single json field, which should contain json
+    representation of the request in dict of the form:
+
+    json = {
+        "api_request": {
+            "type": "POST",
+            "url": "www.xyz.com/api/v1.0/order",
+            "params": {"param_1": "start_time=1"},
+            "headers": {"header_1": "header"},
+            "data": {"data_1": "some_data"},
+            "json": {"json_data": {"json_stuff": "data"}},
+        }
+    }
+    """
+
     def __init__(self):
-        self.session = requests.Session()
         self.reqparse = reqparse.RequestParser()
         self.reqparse.add_argument(
-            "url", type=str, required=True, help="web address to query", location="json"
-        )
-        self.reqparse.add_argument(
-            "params",
-            type=dict,
-            required=False,
-            help="params to append to the url",
-            location="json",
-        )
-        self.reqparse.add_argument(
-            "headers",
-            type=dict,
-            required=False,
-            help="headers to add to the url",
-            location="json",
-        )
-        self.reqparse.add_argument(
-            "data", required=False, help="data to pass form data", location="json"
-        )
-        self.reqparse.add_argument(
-            "json", required=False, help="json data", location="json"
+            "api_request", required=False, help="api_request", location="json"
         )
         super(ApiRequest, self).__init__()
 
     def post(self):
         args = self.reqparse.parse_args(strict=True)
-        logger.debug(args)
-        args["type"] = "POST"
-        prepped = prepare_api_request(args)
+        api_request = ast.literal_eval(args.api_request)
+        prepped = prepare_api_request(api_request)
         with requests.Session() as s:
             result = s.send(prepped, timeout=30)
         return result.text
-
-    def get(self):
-        args = self.reqparse.parse_args(strict=True)
-        args["type"] = "GET"
-        prepped = prepare_api_request(json.dumps(args))
-        with requests.Session() as s:
-            result = s.send(prepped, timeout=30)
-        return result.text
-
-    # def post(self):
-    #     args = self.reqparse.parse_args(strict=True)
-    #     args["type"] = "POST"
-    #     req = requests.Request("POST")
-    #     req.url = args["url"]
-    #     req.headers = {} if args["headers"] is None else args["headers"]
-    #     req.data = [] if args["data"] is None else ast.literal_eval(args["data"])
-    #     req.params = {} if args["params"] is None else ast.literal_eval(args["params"])
-    #     req.json = {} if args["json"] is None else ast.literal_eval(args["json"])
-    #     prepped = req.prepare()
-    #     result = self.session.send(prepped, timeout=30)
-    #     return result.text
-    #
-    # def get(self):
-    #     args = self.reqparse.parse_args(strict=True)
-    #     req = requests.Request("GET")
-    #     req.url = args["url"]
-    #     req.headers = {} if args["headers"] is None else args["headers"]
-    #     req.data = [] if args["data"] is None else ast.literal_eval(args["data"])
-    #     req.params = {} if args["params"] is None else ast.literal_eval(args["params"])
-    #     req.json = {} if args["json"] is None else ast.literal_eval(args["json"])
-    #     prepped = req.prepare()
-    #     result = self.session.send(prepped, timeout=30)
-    #     return result.text
