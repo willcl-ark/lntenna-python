4b3643bfb6b636839cb1e26e21fc462fffcdaffd
willcl-ark <will8clark@gmail.com>
2019-08-14 11:17:38 +0100

    add get_swap_quote()
    Signed-off-by: willcl-ark <will8clark@gmail.com>

---
+++ lntenna/swap/get_swap_quote.py
@@ -0,0 +1,13 @@
+from lntenna.server.config import SUBMARINE_API
+from lntenna.database import db
+import submarine_api
+
+
+def get_swap_quote(uuid: str, invoice: str, network: str):
+    refund_address = db.lookup_refund_addr(uuid)[0]
+    result = submarine_api.get_quote(
+        network=network, invoice=invoice, refund=refund_address
+    )
+    # add the swap to the swap table
+    db.add_swap(uuid=uuid, result=result.json())
+    return {"result": result.text, "status_code": result.status_code}
