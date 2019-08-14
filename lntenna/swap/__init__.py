9acb2a99cf23d42f757f1e119b4c76256256ccff
willcl-ark <will8clark@gmail.com>
2019-08-14 22:10:10 +0100

    add get_address_details()
    Signed-off-by: willcl-ark <will8clark@gmail.com>

--- lntenna/swap/__init__.py
+++ lntenna/swap/__init__.py
@@ -2,6 +2,7 @@ from lntenna.swap.get_address_details import get_address_details
 from lntenna.swap.get_invoice_details import get_invoice_details
 from lntenna.swap.create_order import create_order
 from lntenna.swap.bump_blocksat_order import bump_blocksat_order
+from lntenna.swap.get_address_details import get_address_details
 from lntenna.swap.get_refund_address import get_refund_address
 from lntenna.swap.get_swap_quote import get_swap_quote
 from lntenna.swap.pay_swap import pay_swap
--- lntenna/swap/get_address_details.py
+++ lntenna/swap/get_address_details.py
@@ -4,5 +4,5 @@ from lntenna.swap.utilities import try_json
 
 
 @try_json
-def get_address_details(invoice, network):
-    return submarine_api.get_address_details(SUBMARINE_API, invoice, network)
+def get_address_details(address, network):
+    return submarine_api.get_address_details(SUBMARINE_API, address, network)
