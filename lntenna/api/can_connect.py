b3af622261fe4b0316d1b9dd5693f74d04f7cbf5
willcl-ark <will8clark@gmail.com>
2019-08-19 09:11:10 +0100

    Better help documentation in can_connect Resource
    Signed-off-by: willcl-ark <will8clark@gmail.com>

--- lntenna/api/can_connect.py
+++ lntenna/api/can_connect.py
@@ -8,6 +8,7 @@ from lntenna.gotenna.utilities import check_connection
 
 class CanConnect(Resource):
     def __init__(self):
+        self.help = """Returns whether the GoTenna driver can connect to the device"""
         super(CanConnect, self).__init__()
 
     @check_connection
