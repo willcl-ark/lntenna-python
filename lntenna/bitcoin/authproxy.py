37e9888c58688766d4ee489eb5556d8fffe77ef0
willcl-ark <will8clark@gmail.com>
2019-08-20 15:10:39 +0100

    Remove python-bitcoinlib
    Signed-off-by: willcl-ark <will8clark@gmail.com>

--- lntenna/bitcoin/authproxy.py
+++ lntenna/bitcoin/authproxy.py
@@ -35,7 +35,6 @@ ServiceProxy class:
 
 import base64
 import decimal
-from http import HTTPStatus
 import http.client
 import json
 import logging
@@ -43,6 +42,7 @@ import os
 import socket
 import time
 import urllib.parse
+from http import HTTPStatus
 
 from lntenna.server.config import CONFIG
 
@@ -281,42 +281,3 @@ class AuthServiceProxy:
             self.__conn = http.client.HTTPConnection(
                 self.__url.hostname, port, timeout=self.timeout
             )
-
-
-# class BitcoinProxy:
-#     def __init__(
-#         self,
-#         btc_conf_file=CONFIG["bitcoin"]["BTC_CONF_PATH"],
-#         network=CONFIG["bitcoin"]["BTC_NETWORK"],
-#     ):
-#         self.btc_conf_file = btc_conf_file
-#         self._btc_network = network
-#         bitcoin.SelectParams(self._btc_network)
-#
-#     @property
-#     def network(self):
-#         return self._btc_network
-#
-#     @network.setter
-#     def network(self, network):
-#         """
-#         :param network: one of "mainnet", "testnet" or "regtest"
-#         """
-#         assert network in networks
-#         self._btc_network = network
-#         bitcoin.SelectParams(self.network)
-#
-#     @property
-#     def raw_proxy(self):
-#         return bitcoin.rpc.RawProxy(btc_conf_file=self.btc_conf_file)
-#
-#     @property
-#     def proxy(self):
-#         return bitcoin.rpc.Proxy(btc_conf_file=self.btc_conf_file)
-#
-#     def send_last_tx(self):
-#         """Try to broadcast the last tx in the wallet in case of errors during swap
-#         """
-#         last_txid = self.raw_proxy.listtransactions()[-1]["txid"]
-#         tx_hex = self.raw_proxy.gettransaction(last_txid)["hex"]
-#         self.raw_proxy.sendrawtransaction(tx_hex)
--- lntenna/gotenna/connection.py
+++ lntenna/gotenna/connection.py
@@ -55,20 +55,12 @@ class Connection:
         self.geo_region = None
         self.events = Events()
         self.service_url = None
-        # self.setup_service_url()
         self.btc = btc.AuthServiceProxy()
         self.swap_payment_hash = None
         self.swap_preimage = None
         self.gateway = 0
         self.jumbo_thread = threading.Thread()
         self.cli = False
-
-    # def setup_service_url(self):
-    #     url = f"http://{CONFIG['bitcoin']['RPC_USER']}:"
-    #     url += f"{CONFIG['bitcoin']['RPC_PASSWORD']}@"
-    #     url += f"{CONFIG['bitcoin']['RPC_HOST']}:"
-    #     url += f"{CONFIG['bitcoin']['RPC_PORT']}"
-    #     self.service_url = url
 
     def reset_connection(self):
         if self.api_thread:
